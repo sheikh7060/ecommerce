da550f4947bb97f3c86f221984ea8049ee4e988f
sheikh7060 <mdsheikhsagor@gmail.com>
2025-03-14 11:04:23 +0600

    Generate Html Using js and Make it Interactive

---
+++ amazon.js
@@ -0,0 +1,112 @@
+
+let htmlforEachItem = '';
+products.forEach((productItem)=>{
+
+     htmlforEachItem += `<div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${productItem.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+           ${productItem.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${productItem.rating.stars*10}.png">
+            <div class="product-rating-count link-primary">
+              ${productItem.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            ${((productItem.priceCents)/100).toFixed(2)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-cart" data-product-name="${productItem.name}">
+            Add to Cart
+          </button>
+        </div>`;
+   
+});
+
+document.querySelector('.js-item-grid').innerHTML = htmlforEachItem;
+
+
+document.querySelectorAll('.js-cart')
+    .forEach((cartItem)=>{
+        cartItem.addEventListener('click',()=>{
+            let itemName = cartItem.dataset.productName;
+            addProductToCart(itemName);
+            calculteTotalItem();
+
+
+           
+        
+        })
+    });
+
+
+
+
+
+function addProductToCart(itemName){
+    let matchingItem;
+                    cart.forEach((item)=>{
+                        if(itemName === item.itemName){
+                            matchingItem = item;
+                        }
+
+                    })
+
+                    if(matchingItem){
+                        matchingItem.quantity +=1;
+                    }else{
+                        cart.push({
+                            itemName,
+                            quantity:1
+                        })
+
+                    }
+
+                    console.log(cart);
+
+                    
+                
+};
+
+
+function calculteTotalItem(){
+    let totalItemOrder = 0;
+    cart.forEach((item) =>{
+        totalItemOrder = totalItemOrder + item.quantity;
+
+    })
+
+    console.log(totalItemOrder);
+
+    document.querySelector('.cart-done').innerHTML = totalItemOrder;
+}
